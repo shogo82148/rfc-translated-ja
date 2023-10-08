43de82da20a23d757dc5ac0c8bb43c032b578df5
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-08 17:18:10 +0900

    翻訳のプロンプト改善

--- scripts/translate.py
+++ scripts/translate.py
@@ -16,33 +16,23 @@ def translate(text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-                {"role": "user", "content": "Translate the following English text to Japanese. Note that the input and output are XML texts:\n\n" + text},
+                {
+                    "role": "user",
+                    "content": "You are professional around RFCs. " +
+                        "I will the snippet of RFC text, please translate it into Japanese. " +
+                        'The key words "<bcp14>MUST</bcp14>", "<bcp14>MUST NOT</bcp14>", "<bcp14>REQUIRED</bcp14>", "<bcp14>SHALL</bcp14>", "<bcp14>SHALL NOT</bcp14>", "<bcp14>SHOULD</bcp14>", "<bcp14>SHOULD NOT</bcp14>", "<bcp14>RECOMMENDED</bcp14>", "<bcp14>NOT RECOMMENDED</bcp14>", "<bcp14>MAY</bcp14>", and "<bcp14>OPTIONAL</bcp14>" should be translated into 「<bcp14>しなければなりません（MUST）</bcp14>」、「<bcp14>してはなりません（MUST NOT）</bcp14>」、 「<bcp14>要求されています（REQUIRED）</bcp14>」、 「<bcp14>することになります（SHALL）</bcp14>」、「<bcp14>することはありません（SHALL NOT）</bcp14>」、 「<bcp14>すべきです（SHOULD）</bcp14>」、「<bcp14>すべきではありません（SHOULD NOT）</bcp14>」、 「<bcp14>推奨されます（RECOMMENDED）</bcp14>」、「<bcp14>推奨されません（NOT RECOMMENDED）</bcp14>」、 「<bcp14>してもよいです（MAY）</bcp14>」、「<bcp14>選択できます（OPTIONAL）</bcp14>」.' +
+                        'Note that the input and output are XML texts. Translate the following English text to Japanese:\n\n' + text
+                },
             ]
     )
     translated_text = response['choices'][0]['message']['content']
     print("Output:", translated_text)
     print()
-    return translated_text
+    return text
 
 def stringify_children(node):
     from lxml.etree import tostring
-    from itertools import chain
-    parts = []
-    if node.text:
-        text = node.text
-        text = re.sub('[ \n\r]+', ' ', text)
-        parts.append(text)
-    for c in node:
-        if c.text:
-            text = c.text
-            text = re.sub('\s+', ' ', text)
-            parts.append(text)
-        parts.append(tostring(c, encoding=str, with_tail=False))
-        if c.tail:
-            text = c.tail
-            text = re.sub('\s+', ' ', text)
-            parts.append(text)
-    return ''.join(parts)
+    return tostring(node, encoding=str, with_tail=False)
 
 def translate_node(x):
     s = stringify_children(x)

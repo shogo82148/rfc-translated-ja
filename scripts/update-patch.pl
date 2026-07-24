fac5a8c38e9bb1853302905e856016b8aa98f00f
ICHINOSE Shogo <shogo82148@gmail.com>
2026-07-25 03:56:54 +0900

    RFC 6455: セクション4.1整形

--- scripts/update-patch.pl
+++ scripts/update-patch.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/env perl
 
-use v5.38;
+use v5.44;
+use File::Temp qw(tempdir);
 
-my @rfc_list = glob("./src/en-raw/rfc*.xml");
-my @rfc_numbers = map { m(/rfc([0-9]+)\.xml\z); $1 } @rfc_list;
+my $dir = tempdir(CLEANUP => 1);
+
+my @patch_list = glob("./src/patches/rfc*.patch");
+my @rfc_numbers = map { m(/rfc([0-9]+)\.patch\z); $1 } @patch_list;
 @rfc_numbers = sort { $b <=> $a } @rfc_numbers;
 
 for my $number(@rfc_numbers) {
-    system("diff -u --label '' --label '' ./src/en-raw/rfc$number.xml ./src/ja/rfc$number.xml > ./src/patches/rfc$number.patch");
+    local $ENV{RFC_NO_PATCH} = 1;
+    say "Generating patch for RFC $number...";
+    system("./scripts/txt2xml.pl $number > $dir/rfc$number.xml");
+    system("diff -u --label '' --label '' $dir/rfc$number.xml ./src/en/rfc$number.xml > ./src/patches/rfc$number.patch");
 }
